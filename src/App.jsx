5df52ab43a30f421df52d31a0668285e8f191e57
contrahazte <anamassiel2022@gmail.com>
2024-07-10 01:08:02 +0200

    delete things

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ImageButton from './Components/ImageButton'
+
 const App = () => {
 
   return (
     <div>
-  <ImageButton
-  imageSrc={"https://img.freepik.com/foto-gratis/cerrar-lindo-gato-interior_23-2148882585.jpg?t=st=1720565485~exp=1720569085~hmac=95ade37664cc458ad25ac8866730c7e9bc0243acc0977f03f88a00094ff96cbe&w=740"} />
+      <h1>Hello</h1>
     </div>
 
   )
