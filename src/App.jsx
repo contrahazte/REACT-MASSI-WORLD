7f124f45fad72c66745aeaa5159406519430ecbf
contrahazte <anamassiel2022@gmail.com>
2024-07-10 16:03:40 +0200

    exercise done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Colors } from './Components/Colors'
 
 const App = () => {
+   const colorList = [
+        { name: "red", id: 1 },
+        { name: "blue", id: 2 },
+        { name: "pink", id: 3 }
+    ]
+    return (
+        <div>
+            <Colors
+          colors={colorList}
+            />
+        </div>
 
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-
-  )
+    )
 }
 
 export default App
