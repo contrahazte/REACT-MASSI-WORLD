b080911e8b7451d4e099827532579b723a901dae
contrahazte <anamassiel2022@gmail.com>
2024-07-13 12:10:13 +0200

    exercise done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ShowGithubUser from './Components/ShowGithubUser';
+import GithubUserList from './Components/GithubUserList';
 
-export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/users/johndoe">John Doe</Link>
+          </li>
+          <li>
+            <Link to="/users/janedoe">Jane Doe</Link>
+          </li>
+          <li>
+            <Link to="/users/foobar">Foo Bar</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/users/:username" component={ShowGithubUser} />
+        <Route path="/" component={GithubUserList} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ src/Components/GithubUserList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GithubUserList = () => {
+  return <h2>Add a user and select it</h2>;
+};
+
+export default GithubUserList;
